d3406feb9ab81af086dbbde604d7957eea469461
Marco <marco.teixeiradasilva@hotmail.fr>
2020-08-11 18:13:32 +0200

    fade in

--- scripts/index.js
+++ scripts/index.js
@@ -26,16 +26,48 @@ let timer = setInterval(play, 200)
 const burger = document.querySelector('.burger');
 const menuResp = document.querySelector('.menuResp');
 const body = document.querySelector('.body');
-const liClick = document.querySelector('.clic')
+const liClick = document.querySelector('.clic');
+const menuRespOut = document.querySelector('.menuRespOut')
 
 burger.addEventListener('click', function () {
     menuResp.classList.toggle('active');
+    menuRespOut.classList.toggle('active');
     body.classList.toggle('hidden');
-
 });
+
+menuRespOut.addEventListener('click', function () {
+    menuResp.classList.toggle('active');
+    menuRespOut.classList.toggle('active');
+    body.classList.toggle('hidden');
+})
 
 liClick.addEventListener('click', function() {
     menuResp.classList.remove('active');
     body.classList.remove('hidden');
+    menuRespOut.classList.remove('active');
 });
 
+/********************************** FadeIn *****************************************/
+const ratio = 0.5
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+var observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal').forEach(function (r) {
+    observer.observe(r)
+})
+document.querySelectorAll('.revealTwo').forEach(function (r) {
+    observer.observe(r)
+})
